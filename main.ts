8dd1e75b0b840c18721c806ab14db62d7a46264d
sinyo-matu <sinyo_matu@icloud.com>
2022-07-15 01:45:33 +0900

    update:use light theme

--- main.ts
+++ main.ts
@@ -6,17 +6,17 @@ blog({
   avatar: "https://deno-avatar.deno.dev/avatar/blog.svg",
   avatarClass: "rounded-full",
   author: "An author",
-
+  theme: "light",
   // middlewares: [
 
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
+  // If you want to set up Google Analytics, paste your GA key here.
+  // ga("UA-XXXXXXXX-X"),
 
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
+  // If you want to provide some redirections, you can specify them here,
+  // pathname specified in a key will redirect to pathname in the value.
+  // redirects({
+  //  "/hello_world.html": "/hello_world",
+  // }),
 
   // ]
 });
